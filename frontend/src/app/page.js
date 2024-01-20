fc7cd26ff0cbcd1fa606acaf373b631eb1775a01
im-calvin <kelvinwong0519@gmail.com>
2024-01-20 13:26:50 -0800

    add shadcn fix header imports

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,13 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Header from "./components/header";
+import Header from "@/components/header";
 export default function Home() {
   return (
-    <main >
-      <Header/>
-      <div className="text-3xl " >
-        hello
-      </div>
+    <main>
+      <Header />
+      <div className="text-3xl ">hello</div>
     </main>
   );
 }

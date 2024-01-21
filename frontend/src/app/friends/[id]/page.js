c35947d98ae7c5bf6db261c21ae0a13a7ac692a9
im-calvin <kelvinwong0519@gmail.com>
2024-01-21 02:48:10 -0800

    add dummy url

--- frontend/src/app/friends/[id]/page.js
+++ frontend/src/app/friends/[id]/page.js
@@ -8,6 +8,8 @@ export default function Friends({ params }) {
   //id, email, name, description, image
   let name = "kelvin";
   let description = "hi";
+  let image =
+    "https://firebasestorage.googleapis.com/v0/b/vidshare-2d9f0.appspot.com/o/IMG_20210905_155751.jpg?alt=media&token=8b7b9b9a-9b9a-4b9b-8b9b-9b9b9b9b9b9b";
   return (
     <>
       <div className="flex flex-row">

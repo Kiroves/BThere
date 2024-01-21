c0ac4db332e9ac0c9602e7f7776488561a2349fe
rotalsh <100392201+rotalsh@users.noreply.github.com>
2024-01-20 22:51:24 -0800

    minor edits

--- backend/linked.py
+++ backend/linked.py
@@ -6,8 +6,8 @@ def change_path_separator(file_path):
     return updated_path
 
 # assume images and audio files exist
-imagepath = r"C:\Users\jseok627\Pictures\Camera Roll\WIN_20240120_21_03_43_Pro.jpg"
-audiopath = r"C:\Users\jseok627\Documents\Sound recordings\names.m4a"
+imagepath = ""
+audiopath = ""
 
 imagepath = change_path_separator(imagepath)
 audiopath = change_path_separator(audiopath)
@@ -15,11 +15,6 @@ audiopath = change_path_separator(audiopath)
 emotions = faces.detect_faces_path(imagepath)
 convo = transcribe.transc(audiopath)
 
-print(emotions)
-print(convo)
-
 res = chatgpt.suggestions(emotions, convo)
-
-
 
 print(res)

6aecbf939935f4a18214d3a75cf24311343f558c
im-calvin <kelvinwong0519@gmail.com>
2024-01-21 02:42:56 -0800

    fix page routing

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getDownloadURL, getStorage, ref } from "firebase/storage";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
 export default function Cards({ token, email }) {
   // friends
@@ -22,7 +23,6 @@ export default function Cards({ token, email }) {
       const json = await response.json();
 
       const friends = json.friends;
-      console.log(friends);
 
       const storage = getStorage();
 
@@ -33,12 +33,12 @@ export default function Cards({ token, email }) {
           name: friend.name,
           description: friend.rec,
           image: imgURL, // url
+          id: friend.id,
         };
       });
 
       const fulFilledFriends = await Promise.all(mappedFriends);
 
-      console.log(fulFilledFriends);
       setCards(fulFilledFriends);
     }
     if (token) {
@@ -50,15 +50,17 @@ export default function Cards({ token, email }) {
     <>
       {cards.map((card, idx) => {
         return (
-          <Card key={idx}>
-            <CardHeader>
-              <CardTitle>{card.name}</CardTitle>
-              <CardDescription>{card.description}</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <img src={card.image} width="100" height="100" />
-            </CardContent>
-          </Card>
+          <Link key={card.id} href={`/friends/${card.id}`} >
+            <Card>
+              <CardHeader>
+                <CardTitle>{card.name}</CardTitle>
+                <CardDescription>{card.description}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <img src={card.image} width="100" height="100" />
+              </CardContent>
+            </Card>
+          </Link>
         );
       })}
     </>
